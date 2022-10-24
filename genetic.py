06910b8df2964136b23ee8cb8006681794b77200
Spatika Ganesh <spatikag@ece006.ece.local.cmu.edu>
2022-10-24 01:31:58 -0400

    Mutation estimate cycles

--- genetic.py
+++ genetic.py
@@ -1,10 +1,7 @@
 import random
 from typing import List
-# from hwcounter import Timer, count, count_end
-# import time
-# from pypapi import events, papi_high as high
-import rdtsc
-
+from hwcounter import count, Timer, count_end
+import sys
 
 class Item:
     def __init__(self, name, weight, value):
@@ -16,13 +13,13 @@ class Item:
 class Individual:
     def __init__(self, bits: List[int]):
         self.bits = bits
-
+    
     def __str__(self):
         return repr(self.bits)
 
     def __hash__(self):
         return hash(str(self.bits))
-
+    
     def fitness(self) -> float:
         total_value = sum([
             bit * item.value
@@ -36,16 +33,17 @@ class Individual:
 
         if total_weight <= MAX_KNAPSACK_WEIGHT:
             return total_value
-
+        
         return 0
 
-
+global DURATION
+global OPERATIONS
 MAX_KNAPSACK_WEIGHT = 15
 CROSSOVER_RATE = 0.53
 MUTATION_RATE = 0.013
 REPRODUCTION_RATE = 0.15
-NUMBER_OF_GENERATIONS = 500
-MAX_INITAL_POPULATION = 100
+NUMBER_OF_GENERATIONS = int(sys.argv[1])
+MAX_INITAL_POPULATION = 128
 
 #DON'T CHANGE THIS VALUE
 MAX_CONTENDERS_FOR_TOURNAMENT = 4
@@ -64,13 +62,14 @@ items = [
 ]
 
 
+
 def generate_initial_population(count=MAX_INITAL_POPULATION) -> List[Individual]:
     population = set()
 
     # generate initial population having `count` individuals
     while len(population) != count:
-        # pick random bits one for each item and
-        # create an individual
+        # pick random bits one for each item and 
+        # create an individual 
         bits = [
             random.choice([0, 1])
             for _ in items
@@ -82,19 +81,25 @@ def generate_initial_population(count=MAX_INITAL_POPULATION) -> List[Individual]
 
 def selection(population: List[Individual]) -> List[Individual]:
     parents = []
-
+    
     # randomly shuffle the population
     random.shuffle(population)
-    
-    # we use the first MAX_CONTENDERS_FOR_TOURNAMENT individuals
+
+    # we use the first 4 individuals
     # run a tournament between them and
     # get two fit parents for the next steps of evolution
 
-    for population_index in range(0, MAX_CONTENDERS_FOR_TOURNAMENT - 1, 2):
-        if population[population_index].fitness() > population[population_index + 1].fitness():
-            parents.append(population[population_index])
-        else:
-            parents.append(population[population_index + 1])
+    # tournament between first and second
+    if population[0].fitness() > population[1].fitness():
+        parents.append(population[0])
+    else:
+        parents.append(population[1])
+    
+    # tournament between third and fourth
+    if population[2].fitness() > population[3].fitness():
+        parents.append(population[2])
+    else:
+        parents.append(population[3])
 
     return parents
 
@@ -109,12 +114,22 @@ def crossover(parents: List[Individual]) -> List[Individual]:
 
 
 def mutate(individuals: List[Individual]) -> List[Individual]:
+    global DURATION
+    global OPERATIONS
+    start = count()
     for individual in individuals:
         for i in range(len(individual.bits)):
             if random.random() < MUTATION_RATE:
                 # Flip the bit
                 individual.bits[i] = ~individual.bits[i]
-
+    end = count_end()
+    #print("OPERATIONS BEFORE", OPERATIONS , len(individuals))
+    if len(individuals) > 0 :
+        OPERATIONS += len(individuals) * len(individuals[0].bits)
+    #print("OPERATIONS AFTER", OPERATIONS)
+    #print("DURATION BEFORE", DURATION)
+    DURATION += (end-start)
+    #print("DURATION AFTER", DURATION)
 
 def next_generation(population: List[Individual]) -> List[Individual]:
     next_gen = []
@@ -131,7 +146,7 @@ def next_generation(population: List[Individual]) -> List[Individual]:
             # crossover
             if random.random() < CROSSOVER_RATE:
                 children = crossover(parents)
-
+            
             # mutation
             if random.random() < MUTATION_RATE:
                 mutate(children)
@@ -145,8 +160,7 @@ def print_generation(population: List[Individual]):
     for individual in population:
         print(individual.bits, individual.fitness())
     print()
-    print("Average fitness", sum([x.fitness()
-          for x in population])/len(population))
+    print("Average fitness", sum([x.fitness() for x in population])/len(population))
     print("-" * 32)
 
 
@@ -155,8 +169,6 @@ def average_fitness(population: List[Individual]) -> float:
 
 
 def solve_knapsack() -> Individual:
-    start = rdtsc.get_cycles()
-
     population = generate_initial_population()
 
     avg_fitnesses = []
@@ -166,13 +178,15 @@ def solve_knapsack() -> Individual:
         population = next_generation(population)
 
     population = sorted(population, key=lambda i: i.fitness(), reverse=True)
-
-    end = rdtsc.get_cycles()
-
-    print("Time elapsed during the calculation:", end - start)  
     return population[0]
 
 
 if __name__ == '__main__':
+    global DURATION
+    global OPERATIONS
+    DURATION = 0
+    OPERATIONS = 0
     solution = solve_knapsack()
-    print(solution, solution.fitness())
+    #print(solution, solution.fitness())
+    # No. of ops in one cycle x No. of generations / (throughput x SIMD_length) x Max. freq
+    print( (OPERATIONS * NUMBER_OF_GENERATIONS )/ ()(3.4))
