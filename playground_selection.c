374141274ee99eb027421e5cebb9d1b8027efc18
Sankalp Gunturi <sgunturi@cmu.edu>
2022-11-18 14:23:44 -0800

    Updated the playground_selection code for test run

--- playground_selection.c
+++ playground_selection.c
@@ -67,9 +67,57 @@ double *selection(double *weights, double *values_d, double *initial_population)
 
 int main()
 {
-    double *weights = (double *)malloc((SIZE_OF_INITIAL_POPULATION) * sizeof(double));
-    double *values_d = (double *)malloc((SIZE_OF_INITIAL_POPULATION) * sizeof(double));
-    double *initial_population = (double *)malloc((SIZE_OF_INITIAL_POPULATION) * sizeof(double));
-    selection(weights, values_d, initial_population);
+
+    double weights = [ 8, 5, 1, 0, 4, 4, 9, 10, 15, 3, 12, 10 ];
+    double values_d = [ 1, 10, 5, 15, 9, 11, 0, 13, 13, 8, 4, 2 ];
+    double *winners = (double *)malloc((SIZE_OF_INITIAL_POPULATION / 2) * sizeof(double));
+
+    double *population;
+    int POPULATION_SIZE = 256;
+    int NUMBER_OF_ITEMS = 12;
+    int NUMBER_OF_GENERATIONS = 10000;
+    posix_memalign((void *)&population, 64, POPULATION_SIZE * 12 * sizeof(double));
+
+    srand(time(NULL));
+
+    int skip_index = 15;
+    FILE *fp = fopen("POPULATION_4096.txt", "r");
+
+    if (fp == NULL)
+    {
+        printf("Couldn't open file\n");
+        return 0;
+    }
+
+    char buff[1];
+    for (int i = 0; i < POPULATION_SIZE * NUMBER_OF_ITEMS; i++)
+    {
+        if (i == 0)
+        {
+            for (int k = 0; k < 12 * 14; k++)
+            {
+                fscanf(fp, "%s", buff);
+            }
+            printf("\n");
+        }
+        else if ((i) % 12 == 0)
+        {
+            // After every 12 items
+            for (int k = 0; k < 12 * 14; k++)
+            {
+                fscanf(fp, "%s", buff);
+            }
+            printf("\n");
+        }
+        fscanf(fp, "%s", buff);
+        population[i] = buff[0] - 48.0;
+        // printf(" %.2f ", population[i]);
+    }
+
+    winners = selection(weights, values_d, population);
+    for (int i = 0; i < SIZE_OF_INITIAL_POPULATION / 2; i++)
+    {
+        printf(" %.2f ", winners[i]);
+    }
     return 0;
 }
